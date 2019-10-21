7879f60db3418cf1f99b8c9b67559216df705f02
victorocna <victorocnarescu@gmail.com>
2019-10-21 16:33:40 +0300

    update tailwind config

--- themes/ion/assets/css/tailwind.config.js
+++ themes/ion/assets/css/tailwind.config.js
@@ -1,5 +1,15 @@
 module.exports = {
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        green: "#007b85",
+        blue: "#4252b1",
+      },
+      fontSize: {
+        "7xl": "7rem",
+      },
+    },
+  },
   variants: {},
   plugins: [],
 }
